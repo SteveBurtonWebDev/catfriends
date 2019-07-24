c21e5e90a75655e61a7b531ebe3a6077d1e1d5b5
Steve Burton <steveburtonwebdev@gmail.com>
2019-07-24 09:59:25 +0100

    Updates

---
+++ precache-manifest.23b0836d5c8af8aa8bdc93e29f3355a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fa77539e7502cfe7b8a98b312608e3",
+    "url": "/catfriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/catfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f47ffb77e41b8d4fa4e2",
+    "url": "/catfriends/static/css/main.2ea467e0.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/catfriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "f47ffb77e41b8d4fa4e2",
+    "url": "/catfriends/static/js/main.e71ffa59.chunk.js"
+  },
+  {
+    "revision": "66b7e1568d85b12baeb4",
+    "url": "/catfriends/static/js/runtime~main.2a0eb0af.js"
+  }
+]);
