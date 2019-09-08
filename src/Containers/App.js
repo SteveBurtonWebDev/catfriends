80d14cb78df81a53877c6d696907815a24fa4005
Steve Burton <steveburtonwebdev@gmail.com>
2019-09-08 18:38:55 +0100

    Redux implemented for searchfield

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox'
 import Scroll from '../Components/Scroll'
+import ErrorBoundary from '../Components/ErrorBoundary'
 import './App.css';
+import { setSearchField } from '../actions.js';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
             animals: [],
-            searchfield: ''
         }
     }
     
@@ -18,27 +33,26 @@ class App extends Component {
             .then(response=> response.json())
             .then(users => this.setState({ animals: users}))   
     }
-
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
-    
+   
     render() {
-        const { animals, searchfield } = this.state;
+        const { animals } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredAnimals = animals.filter(animal => {
-            return animal.name.toLowerCase().includes(searchfield.toLowerCase());
+            return animal.name.toLowerCase().includes(searchField.toLowerCase());
         })
         return  !animals.length ? 
             <h1>Loading</h1> :
             <div className ='tc'>
                 <h1 className='f1'>Cat Friends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
-                    <CardList animals={filteredAnimals}/>
+                    <ErrorBoundary>
+                        <CardList animals={filteredAnimals}/>
+                    </ErrorBoundary>
                 </Scroll>
             </div>
             ;
         }
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchCats } from './reducers.js'; 
 import 'tachyons';
 
 
+const store = createStore(searchCats)
+
 ReactDOM.render(
-    <App/>, document.getElementById('root'));
+    <Provider store = {store} >
+      <App />
+    </Provider>  
+      , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
